0846aef6c31cd122c362cb9a51bf96eb61b2a583
Jeff White <jeffreyw@cockroachlabs.com>
2022-07-29 16:59:38 -0500

    More names in the cert

--- scripts/cockroach-create-node.sh
+++ scripts/cockroach-create-node.sh
@@ -7,10 +7,11 @@ set +o allexport
 # get the default IP address of this node
 IP_ADDR=$(ip route get 1 | awk '{print $(NF-2);exit}')
 
-COCKROACH_MORE_NAMES=
+# get the FQDN
+FQDN=$(host -TtA $(hostname -s) | grep "has address" | awk '{print $1}')
 
 if [ "$(whoami)" == "cockroach" ]; then
-	/usr/bin/cockroach cert create-node $COCKROACH_ADVERTISE localhost $IP_ADDR $COCKROACH_MORE_NAMES 
+	/usr/bin/cockroach cert create-node $COCKROACH_ADVERTISE localhost $IP_ADDR $COCKROACH_MORE_NAMES $FQDN 127.0.0.1
 else
-	sudo su - cockroach -c "/usr/bin/cockroach cert create-node $COCKROACH_ADVERTISE localhost $IP_ADDR $COCKROACH_MORE_NAMES"
+	sudo su - cockroach -c "/usr/bin/cockroach cert create-node $COCKROACH_ADVERTISE localhost $IP_ADDR $COCKROACH_MORE_NAMES $FQDN 127.0.0.1"
 fi
